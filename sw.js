532d8ba508ffde0c5ffd6f123f74d5f687bac7a0
hokylhoangkien <phanhoangkien230405@gmail.com>
2025-10-07 18:22:16 +0700

    set up download

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "flappy-animal-v1";
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./audio/flap.mp3",
+  "./audio/hit.mp3",
+  "./audio/background.mp3",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((resp) => resp || fetch(e.request))
+  );
+});
